3ff123ed265993e488a0b8a2ccc128c19ada178e
Santiago Bastidas <santiagobastidase@gmail.com>
2026-05-09 14:59:58 -0500

    Se agregaron validaciones para los campos en el registro para prevenir la entrada de datos incorrectos

--- piedraAzul-back/src/main/java/com/piedrazul/appointments/auth/dto/RegistroPacienteRequest.java
+++ piedraAzul-back/src/main/java/com/piedrazul/appointments/auth/dto/RegistroPacienteRequest.java
@@ -20,21 +20,34 @@ public class RegistroPacienteRequest {
     @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
     private String password;
 
+    @Pattern(
+            regexp = "^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$",
+            message = "Los Nombres solo pueden contener letras y espacios"
+    )
     @NotBlank(message = "Los nombres son obligatorios")
     private String nombres;
-
+    @Pattern(
+            regexp = "^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$",
+            message = "Los Apellidos solo pueden contener letras y espacios"
+    )
     @NotBlank(message = "Los apellidos son obligatorios")
     private String apellidos;
-
+    @Pattern(
+            regexp = "^[0-9]{10}$",
+            message = ("El numero de documento debera tener obligatoriamente 10 digitos")
+    )
     @NotBlank(message = "El número de documento es obligatorio")
     private String numeroDocumento;
-
+    @Pattern(
+            regexp = "^[0-9]{10}$",
+            message = "El celular debe contener exactamente 10 dígitos numéricos"
+    )
     @NotBlank(message = "El celular es obligatorio")
     private String celular;
 
     @NotNull(message = "El género es obligatorio")
     private Genero genero;
-
+    @Past(message = "La fecha de nacimiento debe ser una fecha pasada")
     private LocalDate fechaNacimiento;
 
     @Email(message = "El correo electrónico no es válido")
